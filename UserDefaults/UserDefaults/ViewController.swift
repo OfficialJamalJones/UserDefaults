7d48c7bd84a8259f79de70993360c573d6aab8b3
OfficialJamalJones <jamalamanjones@gmail.com>
2022-12-29 17:44:46 -0500

    Testing Commit

--- UserDefaults/UserDefaults/ViewController.swift
+++ UserDefaults/UserDefaults/ViewController.swift
@@ -5,13 +5,15 @@
 //  Created by Consultant on 12/29/22.
 //
 
+//HW-2: Create an application which will use the UserDefaults to save and retrieve the data. Use different buttons and by tapping on these buttons different data should be printed on a label and after killing and reopening the application it should retrieve the same data in label field.
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Testing")
     }
 
 
